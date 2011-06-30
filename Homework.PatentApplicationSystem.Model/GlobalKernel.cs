96ce1d6b0f81f5154237311e09a57cf91af3b1cb
myrice <qqlddg@gmail.com>
2011-06-30 10:08:03 +0800

    增加数据库帮助类DbHelper与接口IDbHelper
    Signed-off-by: myrice <qqlddg@gmail.com>

---
+++ Homework.PatentApplicationSystem.Model/GlobalKernel.cs
@@ -0,0 +1,9 @@
+﻿using Ninject;
+
+namespace Homework.PatentApplicationSystem.Model
+{
+    public static class GlobalKernel
+    {
+        public static readonly IKernel Instance = new StandardKernel();
+    }
+}
---
+++ Homework.PatentApplicationSystem.Model/IDbHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework.PatentApplicationSystem.Model
+{
+    /// <summary>
+    /// 数据库辅助接口
+    /// </summary>
+    public interface IDbHelper
+    {
+        /// <summary>
+        /// 关闭字符串连接
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// 增删改函数
+        /// 返回:影响的行数
+        /// </summary>
+        /// <param name="sqlString"></param>
+        /// <returns></returns>
+        int AddUpdateDelete(string sqlString);
+    }
+}
