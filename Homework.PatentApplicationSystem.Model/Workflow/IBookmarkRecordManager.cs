1c790fd3f7794960dbb5b4ea0057c9b4ea749ee4
Tom Tung <tom.tung.dyb@gmail.com>
2011-06-30 11:29:03 +0800

    Implemented WorkflowStepService.PendingCaseIds. Add some related classes and interfaces.
    Signed-off-by: Tom Tung <tom.tung.dyb@gmail.com>

---
+++ Homework.PatentApplicationSystem.Model/Workflow/IBookmarkRecordManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace Homework.PatentApplicationSystem.Model.Workflow
+{
+    /// <summary>
+    /// 用于管理案件工作流的书签记录信息。
+    /// </summary>
+    public interface IBookmarkRecordManager
+    {
+        /// <summary>
+        /// 获得记录的所有暂停在书签<param name="bookmarkName"/>处的工作流的ID。
+        /// </summary>
+        IEnumerable<Guid> GetIdsOfAllCaseSuspendedAt(string bookmarkName);
+
+        /// <summary>
+        /// 记录ID为<param name="id"/>的工作流在书签<param name="bookmarkName"/>处暂停。
+        /// </summary>
+        void RecordBookmarkCreated(Guid id, string bookmarkName);
+
+        /// <summary>
+        /// 记录ID为<param name="id"/>的工作流已从书签<param name="bookmarkName"/>处继续。
+        /// </summary>
+        void RecordBookmarkResumed(Guid id, string bookmarkName);
+    }
+}
--- Homework.PatentApplicationSystem.Model/Workflow/WorkflowStepService.cs
+++ Homework.PatentApplicationSystem.Model/Workflow/WorkflowStepService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Ninject;
 
 namespace Homework.PatentApplicationSystem.Model.Workflow
 {
@@ -8,6 +10,9 @@ namespace Homework.PatentApplicationSystem.Model.Workflow
         private readonly string _bookmarkName;
         private readonly User _user;
 
+        /// <remark>
+        /// 假设：参数合法
+        /// </remark>
         internal WorkflowStepService(User user, string bookmarkName)
         {
             _user = user;
@@ -32,7 +37,39 @@ namespace Homework.PatentApplicationSystem.Model.Workflow
 
         public IEnumerable<Guid> PendingCaseIds
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                return GlobalKernel.Instance.Get<IBookmarkRecordManager>()
+                    .GetIdsOfAllCaseSuspendedAt(BookmarkName)
+                    .Where(IsTaskAssignedToMe);
+            }
+        }
+
+        /// <remarks>
+        /// 使用这个方法的假设是：案件编号为<param name="caseId"/>的案件在此对象的书签
+        /// <see cref="WorkflowStepService.BookmarkName"/>处暂停。
+        /// </remarks>
+        private bool IsTaskAssignedToMe(Guid caseId)
+        {
+            // 只有办案员存在被分案的情况
+            if (User.Role != Role.办案员) return true;
+            var @case = GlobalKernel.Instance.Get<ICaseManager>().GetCaseById(caseId);
+            switch (BookmarkName)
+            {
+                case BookmarkNames.撰写五书:
+                case BookmarkNames.定稿五书:
+                case BookmarkNames.客户指示办案:
+                case BookmarkNames.官方来函办案:
+                    return User.UserName == @case.主办员用户名;
+                case BookmarkNames.原始资料翻译:
+                    return User.UserName == @case.翻译用户名;
+                case BookmarkNames.原始资料翻译一校:
+                    return User.UserName == @case.一校用户名;
+                case BookmarkNames.原始资料翻译二校:
+                    return User.UserName == @case.二校用户名;
+                default:
+                    throw new NotSupportedException();
+            }
         }
 
         public bool ContinueCase(Guid caseId, object value)
