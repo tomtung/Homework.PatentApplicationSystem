e0a6a09fef1eba96e3a804e447ed9607bd4feb52
Tom Tung <tom.tung.dyb@gmail.com>
2011-07-01 16:36:10 +0800

    建立了测试项目。
    Signed-off-by: Tom Tung <tom.tung.dyb@gmail.com>

---
+++ Homework.PatentApplicationSystem.Test/DemoTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+using Homework.PatentApplicationSystem.Model;
+
+using NUnit.Framework;
+using FluentAssertions;
+using Moq;
+
+namespace Homework.PatentApplicationSystem.Test
+{
+    [TestFixture]
+    public class DemoTest
+    {
+        [Test]
+        public void SomeTest()
+        {
+            1.Should().Be(1);
+            true.Should().BeTrue();
+
+            Action action = () => { throw new NotSupportedException(); };
+            action.ShouldThrow<NotSupportedException>();
+
+            var mock = new Mock<IUserSpecificService>();
+            mock.SetupGet(s => s.User)
+                .Returns(new User {UserName = "李冬冬", Password = "mystenarice", Role = Role.立案员});
+            Debug.WriteLine(mock.Object.User.UserName);
+        }
+    }
+}
